aec07ba12bf2476b20c68243fc5d1c80d2d11a72
Ryan Towner <townerr@yahoo.com>
2020-11-03 15:24:12 -0600

    Added code to complete AcronymProject

--- Notes/AcronymProject/Program.cs
+++ Notes/AcronymProject/Program.cs
@@ -6,7 +6,16 @@ namespace AcronymProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Get user Input
+            Console.WriteLine("Enter name to get acronym: ");
+            string input = Console.ReadLine();
+            //Split input up
+            string[] strArray = input.Split();
+
+            //loop through each word and get capital first letter and display output
+            foreach (string word in strArray) {
+                Console.Write(word.Substring(0,1).ToUpper());
+            }
         }
     }
 }
