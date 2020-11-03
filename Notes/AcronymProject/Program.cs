46901cf828e36f02657b1b0b540f138c23b1966e
Ryan Towner <townerr@yahoo.com>
2020-11-03 15:11:34 -0600

    Added initial acronym project

---
+++ Notes/AcronymProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AcronymProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
